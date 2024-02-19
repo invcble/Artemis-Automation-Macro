5c0277ff8109b11e15dc30b6705c65571d78c767
invcble <imonbera13@gmail.com>
2024-02-19 14:34:00 -0500

    update

--- Macro Development/client.py
+++ Macro Development/client.py
@@ -1,14 +1,16 @@
 import xmlrpc.client
+import time
 
-s = xmlrpc.client.ServerProxy('http://localhost:8000')
+s = xmlrpc.client.ServerProxy('http://10.250.8.83:4000')
 print(s.multi(2,3))
 print(s.add(2,3))
 print(s.minus(5,2))
 print(s.system.listMethods())
 
-
 # while True:
-#     pass
+#     time.sleep(3)
+#     print(s.status())
+    # pass
 
 
 
--- Macro Development/server.py
+++ Macro Development/server.py
@@ -1,28 +1,30 @@
-import xmlrpc
 from xmlrpc.server import SimpleXMLRPCServer
+import time
 
-server =  SimpleXMLRPCServer(('localhost', 8000))
+server = SimpleXMLRPCServer(('10.250.8.83', 4000))
 server.register_introspection_functions()
+text = "hello"
+
+def status():
+    return text
 
 def add(x, y):
-   return x + y
+    return x + y
 
-def multi(x,y):
-    return x*y
-    
-def minus(x,y):
-    return x-y
+def multi(x, y):
+    return x * y
 
-def client_func():
-    try:
-        client = xmlrpc.client.ServerProxy('http://localhost:8001')
-        client.hello()
-    except Exception:
-        print("ERROR")
+def minus(x, y):
+    return x - y
 
 server.register_function(add)
 server.register_function(multi)
 server.register_function(minus)
-server.register_function(client_func)
+server.register_function(status)
+server.handle_request()
+# server.serve_forever()
 
-server.serve_forever()
+while True:
+    time.sleep(10)
+    text = input("enter the command:\n")
+    server.handle_request()
