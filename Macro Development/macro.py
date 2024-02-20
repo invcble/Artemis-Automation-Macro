b8fd8db53b27bb76d48e0ac4b24d88c3bc4a91e5
invcble <imonbera13@gmail.com>
2024-02-20 17:04:00 -0500

    Update

--- Macro Development/macro.py
+++ Macro Development/macro.py
@@ -2,6 +2,9 @@ import pyautogui as pag
 import time
 import subprocess
 import webbrowser
+import os
+import pygetwindow as gw
+
 
 ####FLOW
 #Server: Start Training
@@ -9,7 +12,7 @@ import webbrowser
 
 #Server: Start Recording
 #Clients: Closes Training.exe,
-########  Starts Kaltura
+########  Starts Kaltura, minimize
 
 #Server: Start Artemis M1
 #Clients: Opens Artemis & Loads Mission 1,
@@ -41,164 +44,171 @@ import webbrowser
 #Clients: Closes browser,
 ########  Stops Kaltura
 
-time.sleep(2)
-training = subprocess.Popen("C:\Program Files\Windows Classics\mspaint.exe") #replace training.exe
-time.sleep(2)
 
-while True:
-    time.sleep(3)
-    if training.poll() is None:
-        True
-    else:
-        print("Starting next sequence")
-        break
-time.sleep(2)
+## AT VERY END, Rename POPUP saves ID, writes log file with that ID,
+## send that ID to participants, participants create a folder with name, MOVES current video files
+
+# time.sleep(2)
+# training = subprocess.Popen("C:\Program Files\Windows Classics\mspaint.exe") #replace training.exe
+# time.sleep(2)
+
+# while True:
+#     time.sleep(3)
+#     if training.poll() is None:
+#         True
+#     else:
+#         print("Starting next sequence")
+#         break
+# time.sleep(2)
 
 #PLACEHOLDER OPEN GUD
 #PLACEHOLDER OPEN File_org
 
-
+time.sleep(4)
 #KALTURA START
+os.system("taskkill /im KalturaCapture.exe /f")
+time.sleep(2)
 pag.write(['win'])
 time.sleep(1)
 pag.write("Kaltura Capture")
 time.sleep(1)
 pag.write(['enter'])
-time.sleep(3)
+time.sleep(15)
 
-try:
-    pag.click(pag.locateCenterOnScreen("new_recording.png", confidence=0.8))
-except pag.ImageNotFoundException:
-    print("Couldn't find New Recording button, proceeding to record")
-time.sleep(2)
-pag.click(pag.locateCenterOnScreen("round_recordbutton.png", confidence=0.8))
-time.sleep(4)
-
-#Record Startup screen coordinates
-
-#SERVER ARTEMIS
-pag.write(['win'])
-time.sleep(1)
-pag.write("Play Artemis")
-time.sleep(1)
-pag.write(['enter'])
-time.sleep(5)
-
-pag.click(340,197) #change
-time.sleep(1)
-pag.click(36,238) #change
-time.sleep(1)
-pag.click(253,552) #change
-time.sleep(5)
-
-pag.click(1728,959) #Startserver
-time.sleep(2)
-pag.click(1537.342) #Modeswitch
-time.sleep(1)
-pag.click(1537,394) #Layoutswitch
-time.sleep(1)
-#WAIT FOR MANUAL START
-# pag.click(1791,1105) #Startgame
-
-#CLIENT ARTEMIS - Helm
-pag.write(['win'])
-time.sleep(1)
-pag.write("Play Artemis")
-time.sleep(1)
-pag.write(['enter'])
-time.sleep(5)
-
-pag.click(340,197) #change
-time.sleep(1)
-pag.click(36,238) #change
-time.sleep(1)
-pag.click(253,552) #change
-time.sleep(5)
-
-pag.click(1741,1029) #Startclient
-time.sleep(2)
-pag.click(1741,1029) #Connectserver
-time.sleep(1)
-pag.click(787,479) #Ship1
-time.sleep(1)
-pag.click(1050,515) #Helm
-time.sleep(1)
-pag.click(1722,1065) #Ready
-
-#CLIENT ARTEMIS - Weapons
-pag.write(['win'])
-time.sleep(1)
-pag.write("Play Artemis")
-time.sleep(1)
-pag.write(['enter'])
-time.sleep(5)
-
-pag.click(340,197) #change
-time.sleep(1)
-pag.click(36,238) #change
-time.sleep(1)
-pag.click(253,552) #change
-time.sleep(5)
-
-pag.click(1741,1029) #Startclient
-time.sleep(2)
-pag.click(1741,1029) #Connectserver
-time.sleep(1)
-pag.click(787,479) #Ship1
-time.sleep(1)
-pag.click(1050,547) #Weapons
-time.sleep(1)
-pag.click(1722,1065) #Ready
-
-#CLIENT ARTEMIS - Engineer
-pag.write(['win'])
-time.sleep(1)
-pag.write("Play Artemis")
-time.sleep(1)
-pag.write(['enter'])
-time.sleep(5)
-
-pag.click(340,197) #change
-time.sleep(1)
-pag.click(36,238) #change
-time.sleep(1)
-pag.click(253,552) #change
-time.sleep(5)
-
-pag.click(1741,1029) #Startclient
-time.sleep(2)
-pag.click(1741,1029) #Connectserver
-time.sleep(1)
-pag.click(787,479) #Ship1
-time.sleep(1)
-pag.click(1050,583) #Engineer
-time.sleep(1)
-pag.click(1722,1065) #Ready
-
-# time.sleep(10)
-# # pag.moveRel(100,100,5)
-# # print(pag.position())
-
-#KALTURA STOP
-pag.moveTo(pag.locateCenterOnScreen("pause.png", confidence=0.8))
-pag.moveRel(-100,0)
+pag.moveTo(638,338) #Startrecord
 pag.mouseDown()
+pag.mouseUp()
+time.sleep(7)
+pag.moveTo(1883,994) #Minimizerecord
 time.sleep(1)
 pag.mouseUp()
+pag.mouseDown()
+pag.mouseUp()
+
+time.sleep(15) #testrecord
+
+#KALTURA STOP
+kaltura = gw.getWindowsWithTitle("Kaltura Capture")[1]
+time.sleep(2)
+
+if kaltura:
+    if kaltura.isMinimized:
+        kaltura.restore()
+    kaltura.activate()
+time.sleep(3)
+
+pag.moveTo(1455,1033) #Stoprecord
 time.sleep(1)
-pag.leftClick()
-time.sleep(2)
-pag.click(pag.locateCenterOnScreen("confirm.png", confidence=0.8))
-time.sleep(2)
-pag.click(pag.locateCenterOnScreen("save_button.png", confidence=0.8))
-time.sleep(2)
-pag.click(pag.locateCenterOnScreen("close_kaltura.png", confidence=0.9))
+pag.mouseUp()
+pag.mouseDown()
+pag.mouseUp()
+pag.moveTo(1510,923) #Stopconfirmation
 time.sleep(1)
-pag.click(pag.locateCenterOnScreen("close_kaltura.png", confidence=0.9))
+pag.mouseUp()
+pag.mouseDown()
+pag.mouseUp()
 
 
-#PLACEHOLDER CLOSE GUD
-#PLACEHOLDER CLOSE File_org
-time.sleep(5)
+# ############ SERVER ARTEMIS ############
+# pag.write(['win'])
+# time.sleep(1)
+# pag.write("Play Artemis")
+# time.sleep(1)
+# pag.write(['enter'])
+# time.sleep(3)
 
-webbrowser.open('https://drexel.qualtrics.com/jfe/form/SV_2gbBTwyemTFj46W')
+# pag.moveTo(264,540) #Startgame
+# pag.mouseDown()
+# pag.mouseUp()
+# time.sleep(5)
+# pag.moveTo(1728,959) #Startserver
+# pag.mouseDown()
+# pag.mouseUp()
+# time.sleep(3)
+# pag.moveTo(1537,342) #Modeswitch
+# pag.mouseDown()
+# pag.mouseUp()
+# time.sleep(2)
+# pag.moveTo(1537,394) #Layoutswitch
+# pag.mouseDown()
+# pag.mouseUp()
+# time.sleep(3)
+# #WAIT FOR MANUAL START
+# #######################################
+
+
+# ############ CLIENT ARTEMIS - Helm ############
+# pag.write(['win'])
+# time.sleep(1)
+# pag.write("Play Artemis")
+# time.sleep(1)
+# pag.write(['enter'])
+# time.sleep(3)
+
+# pag.moveTo(264,540) #Startgame
+# pag.mouseDown()
+# pag.mouseUp()
+# time.sleep(5)
+# pag.moveTo(1741,1029) #Startclient
+# pag.mouseDown()
+# pag.mouseUp()
+# time.sleep(2)
+# pag.moveTo(1741,1029) #Connectserver
+# pag.mouseDown()
+# pag.mouseUp()
+# time.sleep(2)
+# pag.moveTo(787,479) #Ship1
+# pag.mouseDown()
+# pag.mouseUp()
+# time.sleep(1)
+# pag.moveTo(1050,515) #Helm
+# pag.mouseDown()
+# pag.mouseUp()
+# time.sleep(1)
+# pag.moveTo(1722,1065) #Ready
+# pag.mouseDown()
+# pag.mouseUp()
+# time.sleep(3)
+# #READY FOR MANUAL START
+# #######################################
+
+## implement M1 to M2 to M3
+
+############ CLIENT ARTEMIS - Weapons ############
+# pag.click(1050,547) #Weapons
+#READY FOR MANUAL START
+#######################################
+
+############ CLIENT ARTEMIS - Engineer ############
+# pag.click(1050,583) #Engineer
+#READY FOR MANUAL START
+#######################################
+
+
+# # time.sleep(10)
+# # # pag.moveRel(100,100,5)
+# # # print(pag.position())
+
+# #KALTURA STOP
+# pag.moveTo(pag.locateCenterOnScreen("pause.png", confidence=0.8))
+# pag.moveRel(-100,0)
+# pag.mouseDown()
+# time.sleep(1)
+# pag.mouseUp()
+# time.sleep(1)
+# pag.leftClick()
+# time.sleep(2)
+# pag.click(pag.locateCenterOnScreen("confirm.png", confidence=0.8))
+# time.sleep(2)
+# pag.click(pag.locateCenterOnScreen("save_button.png", confidence=0.8))
+# time.sleep(2)
+# pag.click(pag.locateCenterOnScreen("close_kaltura.png", confidence=0.9))
+# time.sleep(1)
+# pag.click(pag.locateCenterOnScreen("close_kaltura.png", confidence=0.9))
+
+
+# #PLACEHOLDER CLOSE GUD
+# #PLACEHOLDER CLOSE File_org
+# time.sleep(5)
+
+# webbrowser.open('https://drexel.qualtrics.com/jfe/form/SV_2gbBTwyemTFj46W')
