7f878466776dac21c139fef90f6bef6713be6b06
invcble <imonbera13@gmail.com>
2024-02-22 02:32:07 -0500

    Update

--- Macro Development/experimenter/experimenter.py
+++ Macro Development/experimenter/experimenter.py
@@ -5,20 +5,48 @@ import subprocess
 # party = xmlrpc.client.ServerProxy('http://10.250.8.83:4000')
 party = xmlrpc.client.ServerProxy('http://localhost:4000')
 
-# print(party.add(2,3))
-# party.status()
-print(party.system.listMethods())
+#  Start Training:   1
+#  Start Recording:  2
+#  Start Artemis M1: 3
+#  Start Survey 1:   4
+#  Start M2:         5
+#  Start Survey 2:   6
+#  Start M3:         7
+#  Start Survey 3:   8
+#  Stop Recording:   9
+#  ID share:         0
+# print(party.system.listMethods())
 
 try:
     while True:
         time.sleep(3)
-        answer = input(" Start Training:   1\n Start Recording:  2\n Start Artemis M1: 3\n Start Survey 1:   4\n Start M2:         5\n Start Survey 2:   6\n Start M3:         7\n Start Survey 3:   8\n Stop Recording:   9\n")
+        answer = input(" Start Training:   1\n Start Recording:  2\n Start Artemis M1: 3\n Start Survey 1:   4\n Start M2:         5\n Start Survey 2:   6\n Start M3:         7\n Start Survey 3:   8\n Stop Recording:   9\n ID share:         0\n")
         if answer == '1':
-            party.actionOne()
-            subprocess.run(['python', 'Macro Development\\experimenter\\test1.py'])
+            party.start_training()
         elif answer == '2':
-            party.actionTwo()
-        else:
+            party.start_recording()
+        elif answer == '3':
+            party.start_artemis_m1()
+            subprocess.run(['python', 'Macro Development\\experimenter\\Start_Artemis_M1_E.py'])
+        elif answer == '4':
+            party.start_survey1()
+            subprocess.run(['python', 'Macro Development\\experimenter\\Take_Screenshot1_E.py'])
+        elif answer == '5':
+            party.start_m2()
+            subprocess.run(['python', 'Macro Development\\experimenter\\Start_M2_E.py'])
+        elif answer == '6':
+            party.start_survey2()
+            subprocess.run(['python', 'Macro Development\\experimenter\\Take_Screenshot2_E.py'])
+        elif answer == '7':
+            party.start_m3()
+            subprocess.run(['python', 'Macro Development\\experimenter\\Start_M3_E.py'])
+        elif answer == '8':
+            party.start_survey3()
+            subprocess.run(['python', 'Macro Development\\experimenter\\Take_Screenshot3_E.py'])
+        elif answer == '9':
+            party.stop_recording()
+            subprocess.run(['python', 'Macro Development\\experimenter\\Stop_Recording_E.py'])
+        elif answer == '0':
             ID = input("Give ID: ")
             party.idShare(ID)
 
--- Macro Development/participant/participant.py
+++ Macro Development/participant/participant.py
@@ -7,21 +7,47 @@ party = SimpleXMLRPCServer(('localhost', 4000), allow_none=True)
 party.register_introspection_functions()
 
 def start_training():
-    subprocess.run(['python', 'Macro Development/participant/test2.py'])
+    subprocess.run(['python', 'Macro Development/participant/Start_Training_P.py'])
 
-def actionTwo():
-    print("HELLO, running test2.py")
-    time.sleep(2)
-    subprocess.run(['python', '\\Macro Development\\test2.py'])
+def start_recording():
+    subprocess.run(['python', 'Macro Development/participant/Start_Recording_P.py'])
+
+def start_artemis_m1():
+    subprocess.run(['python', 'Macro Development/participant/Start_Artemis_M1_P.py'])
+
+def start_survey1():
+    subprocess.run(['python', 'Macro Development/participant/Start_Survey1_P.py'])
+
+def start_m2():
+    subprocess.run(['python', 'Macro Development/participant/Start_M2_P.py'])
+
+def start_survey2():
+    subprocess.run(['python', 'Macro Development/participant/Start_Survey2_P.py'])
+
+def start_m3():
+    subprocess.run(['python', 'Macro Development/participant/Start_M3_P.py'])
+
+def start_survey3():
+    subprocess.run(['python', 'Macro Development/participant/Start_Survey3_P.py'])
+
+def stop_recording():
+    subprocess.run(['python', 'Macro Development/participant/Stop_Recording_P.py'])
 
 def idShare(ID):
     print("Printing ID on server: "+ ID)
 
+
+party.register_function(start_training)
+party.register_function(start_recording)
+party.register_function(start_artemis_m1)
+party.register_function(start_survey1)
+party.register_function(start_m2)
+party.register_function(start_survey2)
+party.register_function(start_m3)
+party.register_function(start_survey3)
+party.register_function(stop_recording)
 party.register_function(idShare)
-party.register_function(actionOne)
-party.register_function(actionTwo)
-# server.handle_request()
-# server.serve_forever()
+
 
 try:
     party.serve_forever()
